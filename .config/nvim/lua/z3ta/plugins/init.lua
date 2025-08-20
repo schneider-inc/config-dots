e5a0060951e66bc2fec846443f2468ba0ae7f07d
schneider-inc <schneiderjus191@gmail.com>
2025-08-20 16:10:09 +0200

    changed up nvim plugins and added wofi-emoji shortcut

--- .config/nvim/lua/z3ta/plugins/init.lua
+++ .config/nvim/lua/z3ta/plugins/init.lua
@@ -1,11 +1,5 @@
 return {
     {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        }
-    },
-    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function ()
--- .config/nvim/lua/z3ta/plugins/lsp.lua
+++ .config/nvim/lua/z3ta/plugins/lsp.lua
@@ -1,65 +1,109 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    dependencies = {
+    {
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-
-        "neovim/nvim-lspconfig",
-
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "L3MON4D3/LuaSnip"
+        config = function()
+            require("mason").setup()
+        end
     },
-    config = function()
-        local lsp_zero = require("lsp-zero")
-        lsp_zero.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
 
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "emmylua_ls",
+                    "rust_analyzer", 
+                    "clangd", 
+                    "basedpyright", 
+                    "arduino_language_server",
+                    "elmls",
+                    "nil_ls",
+                    "ts_ls",
+                    "hyprls",
+                },
+            })
+        end
+    },
 
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "rust_analyzer", 
-                "clangd", 
-                "pyright", 
-            },
-            handlers = {
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require("lspconfig").lua_ls.setup(lua_opts)
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable('emmylua_ls')
+            vim.lsp.enable('rust_analyzer')
+            vim.lsp.enable('clangd')
+            vim.lsp.enable('basedpyright')
+            vim.lsp.enable('arduino_language_server')
+            vim.lsp.enable('elmls')
+            vim.lsp.enable('nil_ls')
+            vim.lsp.enable('ts_ls')
+            vim.lsp.enable('hyprls')
+
+            vim.lsp.config('clangd', {
+                cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+                init_options = {
+                    fallbackFlags = { '-std=c++17' },
+                },
+            })
+            -- vim.lsp.config('
+
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+              vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+              vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+              vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+              -- Use LspAttach autocommand to only map the following keys
+              -- after the language server attaches to the current buffer
+              vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                  -- Enable completion triggered by <c-x><c-o>
+                  vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                  -- Buffer local mappings.
+                  -- See `:help vim.lsp.*` for documentation on any of the below functions
+                  local opts = { buffer = ev.buf }
+                  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                  vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                  end, opts)
+                  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                  vim.keymap.set('n', '<space>f', function()
+
+                  vim.lsp.buf.format { async = true }
+                  end, opts)
                 end,
-            }
-        })
+            })
+        end
+    },
 
-        local cmp = require("cmp")
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
-        cmp.setup({
-            sources = {
-                {name = "path"},
-                {name = "nvim_lsp"},
-                {name = "nvim_lua"},
-            },
-            formatting = lsp_zero.cmp_format(),
-            mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-n"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true}),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-        })
-    end,
+            cmp.setup({
+                sources = {
+                    {name = "path"},
+                    {name = "nvim_lsp"},
+                    {name = "nvim_lua"},
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<C-n"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true}),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+            })
+        end,
+    },
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip"
 }
-
